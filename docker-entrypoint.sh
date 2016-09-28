ab901ff9da1730f1f6ed42a6a95ba8e42d7923fe
peterz3g <peterz3g@163.com>
2016-09-28 13:05:27 +0800

    new

---
+++ docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#/code/manage.py syncdb --noinput
+#/usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
