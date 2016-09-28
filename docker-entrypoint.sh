95286a866ce4b4f464abd81c3ba98cc5288b029a
peterz3g <peterz3g@163.com>
2016-09-28 13:54:47 +0800

    test:

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #/code/manage.py syncdb --noinput
 #/usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python /code/manage.py makemigrations
+python /code/manage.py migrate
+python /code/manage.py runserver 0.0.0.0:8000
 
 
